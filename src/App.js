4ab9ad7281af9d55716e66411eea67b021b6324d
anjor <anjor@umd.edu>
2022-03-18 23:23:45 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -16,19 +16,45 @@ function App() {
   
   React.useEffect(fetchData, [])
 
+  function parseHash(url) {
+    const len = url.length
+    return url.slice(8, len-15)
+  }
+
+  async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
+  function addToCollection(url, collection) {
+    postData("https://anjor-simple-flask-server.herokuapp.com/collections/" + collection, { cid: parseHash(url)})
+    .then(data => console.log(data))
+  }
+
+  function incrementIdx() {
+    setIdx((idx + 1) % images.length)
+  }
+
   return (
     <>
     <div>
       This is a clone of <a href="https://this-person-does-not-exist.com/en">This person does not exist.</a> Except, instead of generating the faces dynamically, the faces are in fact stored on filecoin using <a href="https://estuary.tech/">estuary</a>.
     </div>
-    <button onClick={() => setIdx((idx + 1) % images.length)}>Refresh Image</button>
+    <button onClick={() => incrementIdx()}>Refresh Image</button>
     <br></br>
     <div>
       Identify, and tag the picture as male, female or child.
     </div>
-    <button>Male</button>
-    <button>Female</button>
-    <button>Child</button>
+    <button onClick={() => {addToCollection(images[idx], "male"); incrementIdx()}}>Male</button>
+    <button onClick={() => {addToCollection(images[idx], "female"); incrementIdx()}}>Female</button>
+    <button onClick={() => {addToCollection([images[idx], "child"]); incrementIdx()}}>Child</button>
     <Images images = {images} idx = { idx } />
     </>
   );
